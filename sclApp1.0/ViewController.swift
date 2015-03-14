e810c33155edfc75ad59535cd0310944cef8c46f
System Administrator <root@locadmins-Mac-mini.local>
2015-03-14 21:37:19 +0100

    Initial Commit

---
+++ sclApp1.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sclApp1.0
+//
+//  Created by System Administrator on 14/03/15.
+//  Copyright (c) 2015 System Administrator. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
